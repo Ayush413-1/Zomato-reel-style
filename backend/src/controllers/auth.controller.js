53fafab0f8d0289cbb80d0071c283a79e01da3ca
Ayush Bhongade <your-email@example.com>
2026-07-31 14:40:25 +0530

    fix cors express 5 issue

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,12 +29,12 @@ async function registerUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    });
+   res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message: "User registered successfully",
@@ -73,12 +73,12 @@ async function loginUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    });
+   res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -91,10 +91,11 @@ async function loginUser(req, res) {
 }
 
 function logoutUser(req, res) {
-   res.clearCookie("token", {
+   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none"
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
 });
     res.status(200).json({
         message: "User logged out successfully"
@@ -132,11 +133,11 @@ async function registerFoodPartner(req, res) {
     }, process.env.JWT_SECRET)
 
     res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    });
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message: "Food partner registered successfully",
@@ -179,11 +180,11 @@ async function loginFoodPartner(req, res) {
     }, process.env.JWT_SECRET)
 
     res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    });
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     res.status(200).json({
         message: "Food partner logged in successfully",
